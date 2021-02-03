087f23a072d4241123c441116aceb864f86fd683
moonrailgun <moonrailgun@gmail.com>
2021-02-03 18:02:26 +0800

    feat: sdk增加http请求

--- node/src/index.ts
+++ node/src/index.ts
@@ -2,14 +2,37 @@ import io from 'socket.io-client';
 import md5 from 'md5';
 import { TRPGChatMsgPayload, TRPGUserInfo } from './types';
 import inquirer from 'inquirer';
+import axios, { AxiosInstance } from 'axios';
+import { URL } from 'url';
 
-export { TRPGUserInfo };
+export { TRPGChatMsgPayload, TRPGUserInfo };
 
 export class TRPGClient {
   private socket: SocketIOClient.Socket;
-  constructor(url: string = 'wss://trpgapi.moonrailgun.com') {
+  private request: AxiosInstance;
+  private jwt: string = '';
+  public isLogined = false;
+
+  constructor(public url: string = 'wss://trpgapi.moonrailgun.com') {
     this.socket = io(url, {
       transports: ['websocket'],
+    });
+
+    const urlObj = new URL(this.url);
+    urlObj.protocol = urlObj.protocol === 'wss:' ? 'https:' : 'http:';
+    this.request = axios.create({
+      baseURL: urlObj.toString(),
+    });
+    this.request.interceptors.request.use(async (val) => {
+      if (
+        ['post', 'get'].includes(String(val.method).toLowerCase()) &&
+        !val.headers['X-Token']
+      ) {
+        // 任何请求都尝试增加token
+        val.headers['X-Token'] = this.jwt;
+      }
+
+      return val;
     });
   }
 
@@ -52,7 +75,24 @@ export class TRPGClient {
       password: md5(password),
     });
 
+    this.isLogined = true;
+
+    this.updateJWT(); // 写入jwt
+    setInterval(() => {
+      // 每半天自动更新一次jwt
+      this.updateJWT();
+    }, 1000 * 60 * 60 * 12);
+
     return res.info;
+  }
+
+  async updateJWT() {
+    if (this.isLogined === true) {
+      const { jwt } = await this.send('player::getWebToken');
+      this.jwt = jwt;
+    } else {
+      console.log('获取授权失败, 当前未登录');
+    }
   }
 
   /**
@@ -60,7 +100,7 @@ export class TRPGClient {
    * @param eventName 事件名
    * @param data 数据
    */
-  async send(eventName: string, data: any): Promise<any> {
+  async send(eventName: string, data: any = {}): Promise<any> {
     return new Promise((resolve, reject) => {
       this.socket.emit(eventName, data, ({ result, ...others }) => {
         if (result === false) {
