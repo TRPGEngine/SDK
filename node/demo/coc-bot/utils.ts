6ea1bd3f96bbb09fafedceb58557e3c879af702e
moonrailgun <moonrailgun@gmail.com>
2021-03-04 18:17:25 +0800

    refactor: add calSkillSumWithSubName
    and testcase

---
+++ node/demo/coc-bot/utils.ts
@@ -0,0 +1,29 @@
+import _get from 'lodash/get';
+import _sum from 'lodash/sum';
+
+/**
+ * 用于查找并计算COC7模板中技能对于
+ * 根据专攻名找技能
+ */
+export function calSkillSumWithSubName(
+  actorData: object,
+  skillName: string,
+  skillSubName?: string
+): number {
+  const result = Object.entries(actorData)
+    // 过滤出所有技能数据
+    .filter(([key]) => key.startsWith('skill-'))
+    .find(([key, value]) => {
+      if (key.includes(skillName)) {
+        if (typeof skillSubName === 'string') {
+          return _get(value, ['专攻']) === skillSubName;
+        } else {
+          return true;
+        }
+      }
+
+      return false;
+    });
+
+  return _sum([0, _get(result, [1, '基础']), _get(result, [1, 'addedPoint'])]);
+}
